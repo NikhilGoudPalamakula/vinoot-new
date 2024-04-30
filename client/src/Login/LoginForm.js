c9c69c2c5da50b72b6bde63f8038a1275219df0f
NikhilGoudPalamakula <nikhilgoud2807@gmail.com>
2024-04-30 23:25:37 +0530

    push

--- client/src/Login/LoginForm.js
+++ client/src/Login/LoginForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import './LoginForm.css';
-import axios from "axios"; 
+import axios from "axios";
 const LoginForm = () => {
   const [isSignInActive, setIsSignInActive] = useState(false);
 
@@ -64,16 +64,16 @@ const LoginForm = () => {
       // Optionally, you can display an error message to the user
     }
   };
-    // ---------------------------------------------------------------
-    // ----------------Super-admin-login------------\
+  // ---------------------------------------------------------------
+  // ----------------Super-admin-login------------\
 
 
-    const [formData1, setFormData1] = useState({
+  const [formData1, setFormData1] = useState({
     username: '',
     password: '',
   });
 
-  const handleChange1 = (e) => {
+  const handleChange12 = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
@@ -83,7 +83,7 @@ const LoginForm = () => {
       const res = await axios.post('http://localhost:5001/api/login', formData);
       console.log('User logged in:', res.data);
       localStorage.setItem('username', res.data.username);
-        navigate("/FrSidebar");
+      navigate("/FrSidebar");
       // Optionally, you can redirect the user to another page after successful login
     } catch (error) {
       console.error('Login failed:', error.response.data.error);
@@ -91,14 +91,14 @@ const LoginForm = () => {
     }
   };
 
-    // ----------------------------------------------------------------
+  // ----------------------------------------------------------------
 
   return (
     <div className='total'>
-    <div className={`container ${isSignInActive ? 'right-panel-active' : ''}`}>
-      <div className="container-form signUp">
-        {/* -----------SuperAdmin---------- */}
-        <form className="form">
+      <div className={`container ${isSignInActive ? 'right-panel-active' : ''}`}>
+        <div className="container-form signUp">
+          {/* -----------SuperAdmin---------- */}
+          {/* <form className="form">
           <h2 className="title">Super Admin</h2>
           <input 
            className="input" 
@@ -113,58 +113,82 @@ const LoginForm = () => {
           className="input" 
           type="password"
           name="password"
-          value={formData.password}
+          value={formData1.password}
           onChange={handleChange1}
           placeholder="Password"
           required />
           <button className="btn" type='submit' onClick={handleSubmit1}>Login</button>
-        </form>
-      </div>
+        </form> */}
 
-      <div className="container-form signIn">
-         {/* -----------Franchise Staff---------- */}
-        <form className="form">
-          <h2 className="title">Franchise Staff</h2>
-          {/* <input type="text" className="input" placeholder="User" /> */}
-          <input 
-          className="input" 
-          type="text"
-          name="Adminid"
-          value={formData.Adminid}
-          onChange={handleChange}
-          placeholder="Username"
-          required />
+          <form className="form">
+            <h2 className="title">Super Admin</h2>
+            <input
+              className="input"
+              type="text"
+              name="username"
+              value={formData1.username}
+              onChange={handleChange12} 
+              placeholder="Username"
+              required
+            />
 
-          <input 
-          className="input" 
-          type="text"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          placeholder="Password"
-          required />
-          <a href="/fr" className="link">
-            Do you want to register for <span style={{textDecoration:'underline',color:'green'}}>Franchise?</span>
-          </a>
-          <button className="btn" type='submit' onClick={handleSubmit}>Login</button>
-        </form>
-      </div>
+            <input
+              className="input"
+              type="password"
+              name="password"
+              value={formData1.password}
+              onChange={handleChange12}
+              placeholder="Password"
+              required
+            />
+            <button className="btn" type='submit' onClick={handleSubmit1}>Login</button>
+          </form>
+        </div>
 
-      <div className="container-overlay">
-        <div className="overlay">
-          <div className="overlay-panel overlay-left">
-            <button className="btn" onClick={toggleSignIn}>
-              Franchise Staff 
-            </button>
-          </div>
-          <div className="overlay-panel overlay-right">
-            <button className="btn" onClick={toggleSignUp}>
-             Super Admin
-            </button>
+        <div className="container-form signIn">
+          {/* -----------Franchise Staff---------- */}
+          <form className="form">
+            <h2 className="title">Franchise Staff</h2>
+            {/* <input type="text" className="input" placeholder="User" /> */}
+            <input
+              className="input"
+              type="text"
+              name="Adminid"
+              value={formData.Adminid}
+              onChange={handleChange}
+              placeholder="Username"
+              required />
+
+            <input
+              className="input"
+              type="text"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              placeholder="Password"
+              required />
+            <a href="/fr" className="link">
+              Do you want to register for <span style={{ textDecoration: 'underline', color: 'green' }}>Franchise?</span>
+            </a>
+            <button className="btn" type='submit' onClick={handleSubmit}>Login</button>
+          </form>
+        </div>
+
+        <div className="container-overlay">
+          <div className="overlay">
+            <div className="overlay-panel overlay-left">
+              <button className="btn" onClick={toggleSignIn}>
+                Franchise Staff
+              </button>
+            </div>
+            <div className="overlay-panel overlay-right">
+              <button className="btn" onClick={toggleSignUp}>
+                Super Admin
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };

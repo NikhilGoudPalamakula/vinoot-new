83e7cc7bef56cebc17b0a228fe97c1f40b4a7f12
vikas90322 <132671498+vikas90322@users.noreply.github.com>
2024-05-01 14:55:41 +0530

    ii

--- server/routes/FranchiseRoutes.js
+++ server/routes/FranchiseRoutes.js
@@ -13,35 +13,3 @@ router.get('/doctors', adminController.getDoctors);
 
 module.exports = router;
 
-
-
-// const express = require('express');
-// const router = express.Router();
-// const franchiseController = require('../controllers/FranchiseRegController');
-// const adminController = require('../controllers/FranchiseAdminController');
-
-// router.post('/franchise', franchiseController.createFranchise);
-// router.post('/admin', adminController.createAdmin);
-// router.post('/franchiselogin', adminController.loginfranchiseUser);
-// router.get('/franchisefetch', adminController.getAllFranchiseAmins);
-// // router.patch('/franchisestateupdate/:id', adminController.updateFranchiseAdminActiveState);
-// router.patch('/updateFranchiseAdmin/:id', franchiseController.updateFranchiseAdmin);
-// router.delete('/deleteFranchiseAdmin/:id', adminController.deleteAdmin);
-
-// module.exports = router;
-
-
-
-
-// const express = require('express');
-// const router = express.Router();
-// const franchiseAdminController = require('../controllers/FranchiseAdminController');
-
-// router.post('/franchise', franchiseController.createFranchise);
-// router.post('/admin', franchiseAdminController.createAdmin);
-// router.get('/franchisefetch', franchiseAdminController.getAllFranchiseAmins);
-// router.post('/franchiselogin', franchiseAdminController.loginfranchiseUser);
-// router.patch('/updateFranchiseAdmin/:id', franchiseAdminController.updateFranchiseAdmin);
-// router.delete('/deleteFranchiseAdmin/:id', franchiseAdminController.deleteAdmin);
-
-// module.exports = router;

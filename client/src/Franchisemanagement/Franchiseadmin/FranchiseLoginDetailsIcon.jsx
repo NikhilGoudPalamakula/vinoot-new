b5f113923f6b16142511f608999c87406a2b88bb
vikas90322 <132671498+vikas90322@users.noreply.github.com>
2024-05-05 13:53:58 +0530

    hii

---
+++ client/src/Franchisemanagement/Franchiseadmin/FranchiseLoginDetailsIcon.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import React from 'react'
+import './FranchiseLoginDetailsIcon.css'
+
+import { FaRegCircleUser } from "react-icons/fa6";
+
+const FranchiseLoginDetailsIcon = () => {
+
+    const [showDropdown, setShowDropdown] = useState(false);
+    const createdBy = localStorage.getItem("userId");
+    const franchiseName = localStorage.getItem("franchisename");
+    const FranchiseiD = localStorage.getItem("FranchiseID");
+
+    const toggleDropdown = () => {
+        setShowDropdown(!showDropdown);
+      };
+    
+
+
+  return (
+    <div className="franchise-login-details-icon-container-vik">
+    <div onClick={toggleDropdown} className="icon-container-vik">
+        <FaRegCircleUser />
+    </div>
+    {showDropdown && (
+        <div className="dropdown-vik">
+          <p>Franchise Name: {franchiseName}</p>
+          <p>Franchise ID: {FranchiseiD}</p>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default FranchiseLoginDetailsIcon

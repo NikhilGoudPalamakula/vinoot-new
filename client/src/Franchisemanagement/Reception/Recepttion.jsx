3f7dca22f0f432beaa8c375db96c0421288be817
manoj66416 <58281743+manoj66416@users.noreply.github.com>
2024-05-06 21:35:23 +0530

    billingdata pagination

--- client/src/Franchisemanagement/Reception/Recepttion.jsx
+++ client/src/Franchisemanagement/Reception/Recepttion.jsx
@@ -1,12 +1,18 @@
+import React, { useState, useEffect } from "react";
+import ReceptionSidebar from "./ReceptionSidebar/ReceptionSidebar";
+import axios from "axios";
+import "./Recepttion.css";
 
-
-import React, { useState, useEffect } from 'react';
-import ReceptionSidebar from './ReceptionSidebar/ReceptionSidebar';
-import axios from 'axios';
-import './Recepttion.css'
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
+import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
 
 const Reception = () => {
   const [billingData, setBillingData] = useState([]);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(3);
 
   useEffect(() => {
     const fetchBillingData = async () => {
@@ -21,22 +27,35 @@ const Reception = () => {
           console.error("FranchiseID not found in localStorage");
         }
       } catch (error) {
-        console.error('Error fetching billing data:', error);
+        console.error("Error fetching billing data:", error);
       }
     };
-  
+
     fetchBillingData();
   }, []);
-  
 
+  // Pagination handlers
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  // Get current plans
+  const indexOfLastPlan = currentPage * itemsPerPage;
+  const indexOfFirstPlan = indexOfLastPlan - itemsPerPage;
+  const currentPlans = billingData.slice(indexOfFirstPlan, indexOfLastPlan);
+
+  // Calculate total pages
+  const totalPages = Math.ceil(billingData.length / itemsPerPage);
   return (
-    <div className='recp-total'>
-      <div><ReceptionSidebar /></div>
-      <div  className='recp-dash-table'>
+    <div className="recp-total">
+      <div>
+        <ReceptionSidebar />
+      </div>
+      <div className="recp-dash-table">
         <h1>Patients Billing Details</h1>
         <table>
           <thead>
-            <tr> 
+            <tr>
               <th>Date</th>
               <th>Bill Number</th>
               <th>Patient Name</th>
@@ -51,27 +70,49 @@ const Reception = () => {
             </tr>
           </thead>
           <tbody>
-            {billingData.map((billing) => (
+            {currentPlans.map((billing) => (
               <tr key={billing._id}>
-                  <td>{billing.currentDate}</td>
-                  <td>{billing.bill_number}</td>
-                  <td>{billing.patient_name}</td>
-                  <td>{billing.mobile_number}</td>
-                  <td>{billing.doctor}</td>
+                <td>{billing.currentDate}</td>
+                <td>{billing.bill_number}</td>
+                <td>{billing.patient_name}</td>
+                <td>{billing.mobile_number}</td>
+                <td>{billing.doctor}</td>
                 <td>{billing.plan_name}</td>
                 <td>{billing.days}</td>
-                  <td>{billing.price}</td>
-                  <td>{billing.amountPaid}</td>
+                <td>{billing.price}</td>
+                <td>{billing.amountPaid}</td>
                 <td>{billing.remainingAmount}</td>
                 {/* <td>{billing.remainingAmount}</td> */}
               </tr>
             ))}
           </tbody>
         </table>
+        <div className="paginationss">
+          <span onClick={() => handlePageChange(1)}>
+            <KeyboardDoubleArrowLeftIcon />
+          </span>
+          <span onClick={() => handlePageChange(currentPage - 1)}>
+            <KeyboardArrowLeftIcon />
+          </span>
+          {[...Array(totalPages)].map((_, index) => (
+            <span
+              key={index}
+              onClick={() => handlePageChange(index + 1)}
+              className={currentPage === index + 1 ? "pageactive-page" : ""}
+            >
+              {index + 1}
+            </span>
+          ))}
+          <span onClick={() => handlePageChange(currentPage + 1)}>
+            <KeyboardArrowRightIcon />
+          </span>
+          <span onClick={() => handlePageChange(totalPages)}>
+            <KeyboardDoubleArrowRightIcon />
+          </span>
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default Reception;
-

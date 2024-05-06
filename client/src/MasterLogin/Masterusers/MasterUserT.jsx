93575e4d8a869dfd9db1492a6498f16930abc88b
manoj66416 <58281743+manoj66416@users.noreply.github.com>
2024-05-06 18:38:59 +0530

    superadmin pagination

--- client/src/MasterLogin/Masterusers/MasterUserT.jsx
+++ client/src/MasterLogin/Masterusers/MasterUserT.jsx
@@ -1,16 +1,22 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./MasterUserT.css";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
+import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
 const MasterUserT = () => {
   const [users, setUsers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(3);
 
   useEffect(() => {
     fetchUsers();
-  }, []);
+  }, [currentPage]);
 
   const fetchUsers = async () => {
     try {
-      const response = await axios.get("http://localhost:5001/api/users");
+      const response = await axios.get(`http://localhost:5001/api/users`);
       setUsers(response.data);
     } catch (error) {
       console.error("Error fetching users:", error);
@@ -30,6 +36,17 @@ const MasterUserT = () => {
       console.error("Error updating active state:", error);
     }
   };
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  // Get current plans
+  const indexOfLastPlan = currentPage * itemsPerPage;
+  const indexOfFirstPlan = indexOfLastPlan - itemsPerPage;
+  const currentPlans = users.slice(indexOfFirstPlan, indexOfLastPlan);
+
+  // Calculate total pages
+  const totalPages = Math.ceil(users.length / itemsPerPage);
   return (
     <>
       <div>
@@ -50,7 +67,7 @@ const MasterUserT = () => {
             </tr>
           </thead>
           <tbody>
-            {users.map((user) => (
+            {currentPlans.map((user) => (
               <tr key={user._id}>
                 <td>{user.fullName}</td>
                 <td>{user.userId}</td>
@@ -73,6 +90,29 @@ const MasterUserT = () => {
             ))}
           </tbody>
         </table>
+        <div className="paginationss">
+          <span onClick={() => handlePageChange(1)}>
+            <KeyboardDoubleArrowLeftIcon />
+          </span>
+          <span onClick={() => handlePageChange(currentPage - 1)}>
+            <KeyboardArrowLeftIcon />
+          </span>
+          {[...Array(totalPages)].map((_, index) => (
+            <span
+              key={index}
+              onClick={() => handlePageChange(index + 1)}
+              className={currentPage === index + 1 ? "pageactive-page" : ""}
+            >
+              {index + 1}
+            </span>
+          ))}
+          <span onClick={() => handlePageChange(currentPage + 1)}>
+            <KeyboardArrowRightIcon />
+          </span>
+          <span onClick={() => handlePageChange(totalPages)}>
+            <KeyboardDoubleArrowRightIcon />
+          </span>
+        </div>
       </div>
     </>
   );

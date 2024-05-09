e2c04f099a01b44b7d866e59d7ce4335c6760b77
NikhilGoudPalamakula <nikhilgoud2807@gmail.com>
2024-05-09 20:30:00 +0530

    push

--- client/src/Franchisemanagement/Reception/Recepttion.jsx
+++ client/src/Franchisemanagement/Reception/Recepttion.jsx
@@ -1,7 +1,9 @@
+
 // import React, { useState, useEffect } from "react";
 // import ReceptionSidebar from "./ReceptionSidebar/ReceptionSidebar";
 // import axios from "axios";
 // import "./Recepttion.css";
+// import { useNavigate } from "react-router-dom"; // import ShowPatientDetails from "./Patientdetails/ShowPatientDetails";
 
 // import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 // import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
@@ -10,9 +12,11 @@
 
 // const Reception = () => {
 //   const [billingData, setBillingData] = useState([]);
+//   // const [selectedPatientId, setSelectedPatientId] = useState(null); // State to store selected patient ID
 
 //   const [currentPage, setCurrentPage] = useState(1);
 //   const [itemsPerPage] = useState(3);
+//   const navigate = useNavigate();
 
 //   useEffect(() => {
 //     const fetchBillingData = async () => {
@@ -38,6 +42,9 @@
 //   const handlePageChange = (pageNumber) => {
 //     setCurrentPage(pageNumber);
 //   };
+//   const handleDetailsClick = async (patientId) => {
+//     navigate(`/showPatient/${patientId}`);
+//   };
 
 //   // Get current plans
 //   const indexOfLastPlan = currentPage * itemsPerPage;
@@ -53,6 +60,28 @@
 //       </div>
 //       <div className="recp-dash-table">
 //         <h1>Patients Billing Details</h1>
+
+
+//         <label>
+//         <span>from date </span>
+//         <input type="date"></input>
+//       </label>
+//       <label>
+//         <span>to date</span>
+//         <input type="date"></input>
+//       </label>
+//       <label>
+//         <span>Plan type</span>
+//         <input></input>
+//       </label>
+//       <label>
+//         <span>mobile number</span>
+//         <input type="number"></input>
+//       </label>
+//       <label>
+//         <span>Remaining Amount</span>
+//         <input></input>
+//       </label>
 //         <table>
 //           <thead>
 //             <tr>
@@ -79,10 +108,15 @@
 //                 <td>{billing.doctor}</td>
 //                 <td>{billing.plan_name}</td>
 //                 <td>{billing.days}</td>
-//                 <td>{billing.price}</td>
+//                 <td>{billing.TotalAmount}</td>
 //                 <td>{billing.amountPaid}</td>
 //                 <td>{billing.remainingAmount}</td>
-//                 <td><button>Details</button></td>
+//                 <td>
+//                   <button
+//                     onClick={() => handleDetailsClick(billing.patient_id)}>
+//                     Details
+//                   </button>
+//                 </td>
 //               </tr>
 //             ))}
 //           </tbody>
@@ -98,8 +132,7 @@
 //             <span
 //               key={index}
 //               onClick={() => handlePageChange(index + 1)}
-//               className={currentPage === index + 1 ? "pageactive-page" : ""}
-//             >
+//               className={currentPage === index + 1 ? "pageactive-page" : ""}>
 //               {index + 1}
 //             </span>
 //           ))}
@@ -119,13 +152,11 @@
 
 
 
-
 import React, { useState, useEffect } from "react";
 import ReceptionSidebar from "./ReceptionSidebar/ReceptionSidebar";
 import axios from "axios";
 import "./Recepttion.css";
-import { useNavigate } from "react-router-dom"; // import ShowPatientDetails from "./Patientdetails/ShowPatientDetails";
-
+import { useNavigate } from "react-router-dom";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
@@ -133,10 +164,16 @@ import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArro
 
 const Reception = () => {
   const [billingData, setBillingData] = useState([]);
-  // const [selectedPatientId, setSelectedPatientId] = useState(null); // State to store selected patient ID
-
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage] = useState(3);
+  const [filters, setFilters] = useState({
+    fromDate: "",
+    toDate: "",
+    mobileNumber: "",
+    planType: "",
+    patientname: "",
+    remainingAmount: ""
+  });
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -159,21 +196,42 @@ const Reception = () => {
     fetchBillingData();
   }, []);
 
-  // Pagination handlers
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
+
   const handleDetailsClick = async (patientId) => {
     navigate(`/showPatient/${patientId}`);
   };
 
-  // Get current plans
+  const filteredData = billingData.filter(billing => {
+
+    const remainingAmount = parseFloat(billing.remainingAmount);
+    const filterValue = parseFloat(filters.remainingAmount);
+    const lowercaseName = filters.patientname.toLowerCase();
+    const lowercaseBillingName = billing.patient_name.toLowerCase();
+    return (
+      billing.currentDate.includes(filters.fromDate) &&
+      billing.currentDate.includes(filters.toDate) &&
+      billing.mobile_number.toString().includes(filters.mobileNumber) &&
+      billing.plan_name.includes(filters.planType) &&
+      lowercaseBillingName.includes(lowercaseName) &&
+      // billing.remainingAmount.toString().includes(filters.remainingAmount)
+      billing.plan_name.includes(filters.planType) &&
+      (isNaN(filterValue) || remainingAmount >= filterValue)
+    );
+  });
+
   const indexOfLastPlan = currentPage * itemsPerPage;
   const indexOfFirstPlan = indexOfLastPlan - itemsPerPage;
-  const currentPlans = billingData.slice(indexOfFirstPlan, indexOfLastPlan);
+  const currentPlans = filteredData.slice(indexOfFirstPlan, indexOfLastPlan);
+  const totalPages = Math.ceil(filteredData.length / itemsPerPage);
 
-  // Calculate total pages
-  const totalPages = Math.ceil(billingData.length / itemsPerPage);
+  const handleFilterChange = (e) => {
+    const { name, value } = e.target;
+    setFilters({ ...filters, [name]: value });
+  };
+
   return (
     <div className="recp-total">
       <div>
@@ -181,13 +239,67 @@ const Reception = () => {
       </div>
       <div className="recp-dash-table">
         <h1>Patients Billing Details</h1>
+        <label>
+          <span>From Date:</span>
+          <input
+            type="date"
+            name="fromDate"
+            value={filters.fromDate}
+            onChange={handleFilterChange}
+          />
+        </label>
+        <label>
+          <span>To Date:</span>
+          <input
+            type="date"
+            name="toDate"
+            value={filters.toDate}
+            onChange={handleFilterChange}
+          />
+        </label>
+        <label>
+          <span>Mobile Number:</span>
+          <input
+            type="text"
+            name="mobileNumber"
+            value={filters.mobileNumber}
+            onChange={handleFilterChange}
+          />
+        </label>
+        <label>
+          <span>Name:</span>
+          <input
+            type="text"
+            name="patientname"
+            value={filters.patientname}
+            onChange={handleFilterChange}
+          />
+        </label>
+        <label>
+          <span>Plan Type:</span>
+          <input
+            type="text"
+            name="planType"
+            value={filters.planType}
+            onChange={handleFilterChange}
+          />
+        </label>
+        <label>
+          <span>Remaining Amount:</span>
+          <input
+            type="text"
+            name="remainingAmount"
+            value={filters.remainingAmount}
+            onChange={handleFilterChange}
+          />
+        </label>
         <table>
           <thead>
             <tr>
               <th>Date</th>
               <th>Bill Number</th>
               <th>Patient Name</th>
-              <th>Patient MobileNumber</th>
+              <th>Patient Mobile Number</th>
               <th>Doctor</th>
               <th>Plan Type</th>
               <th>Days</th>
@@ -231,7 +343,8 @@ const Reception = () => {
             <span
               key={index}
               onClick={() => handlePageChange(index + 1)}
-              className={currentPage === index + 1 ? "pageactive-page" : ""}>
+              className={currentPage === index + 1 ? "pageactive-page" : ""}
+            >
               {index + 1}
             </span>
           ))}
@@ -248,3 +361,4 @@ const Reception = () => {
 };
 
 export default Reception;
+

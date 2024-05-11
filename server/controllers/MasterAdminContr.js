0554b299a565d4eabe4d7726c0ccb63ab15d80ed
manoj66416 <58281743+manoj66416@users.noreply.github.com>
2024-05-11 13:26:36 +0530

    superAdmin crud operation

--- server/controllers/MasterAdminContr.js
+++ server/controllers/MasterAdminContr.js
@@ -1,4 +1,4 @@
-const User = require('../models/MasterAdminRegmodel');
+const User = require("../models/MasterAdminRegmodel");
 
 // Controller to handle user registration
 exports.registerUser = async (req, res) => {
@@ -7,12 +7,12 @@ exports.registerUser = async (req, res) => {
     res.status(201).json(user);
   } catch (error) {
     if (error.code === 11000 && error.keyPattern.userId) {
-      res.status(400).json({ error: 'User ID already exists' });
+      res.status(400).json({ error: "User ID already exists" });
     } else if (error.code === 11000 && error.keyPattern.phoneNumber) {
-      res.status(400).json({ error: 'Phone number already exists' });
+      res.status(400).json({ error: "Phone number already exists" });
     } else {
       console.error(error);
-      res.status(500).json({ error: 'Server Error' });
+      res.status(500).json({ error: "Server Error" });
     }
   }
 };
@@ -22,19 +22,17 @@ exports.loginUser = async (req, res) => {
   try {
     const user = await User.findOne({ userId, password });
     if (!user) {
-      return res.status(400).json({ error: 'Invalid credentials' });
+      return res.status(400).json({ error: "Invalid credentials" });
     }
     if (!user.isActive) {
-      return res.status(400).json({ error: 'User is not active' });
+      return res.status(400).json({ error: "User is not active" });
     }
     res.status(200).json(user);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Server Error' });
+    res.status(500).json({ error: "Server Error" });
   }
 };
-
-
 
 exports.getAllUsers = async (req, res) => {
   try {
@@ -42,24 +40,36 @@ exports.getAllUsers = async (req, res) => {
     res.status(200).json(users);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Server Error' });
+    res.status(500).json({ error: "Server Error" });
   }
 };
 
 exports.updateUserActiveState = async (req, res) => {
   const { id } = req.params;
-  const { isActive, updatedBy } = req.body; // Updated by information
+  const { isActive, updatedBy, fullName, email, password } = req.body; // Updated by information
   try {
     // Update active state and updated by information
-    await User.findByIdAndUpdate(id, { isActive, modifiedBy: updatedBy, modifiedAt: Date.now() });
-    res.status(200).json({ message: 'User active state updated successfully' });
+    const updatedAdmin = await User.findByIdAndUpdate(
+      id,
+      {
+        isActive,
+        modifiedBy: updatedBy,
+        modifiedAt: Date.now(),
+        fullName,
+        email,
+        password,
+      },
+      { new: true }
+    );
+    if (!updatedAdmin) {
+      return res.status(404).json({ error: "Admin not found" });
+    }
+    res.status(200).json({ message: "User active state updated successfully" });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Server Error' });
+    res.status(500).json({ error: "Server Error" });
   }
 };
-
-
 
 // const User = require('../models/MasterAdminRegmodel');
 
